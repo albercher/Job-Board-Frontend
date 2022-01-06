dd94b1040740c1ab5cf2eaf65f691bd149f6325b
Abbigale Bercher <abbeybercher@gmail.com>
2022-01-06 14:18:18 -0500

    added back top top button

---
+++ src/components/BackToTop.js
@@ -0,0 +1,55 @@
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import Zoom from '@mui/material/Zoom';
+import { Fragment } from 'react';
+
+function ScrollTop(props) {
+  const { children } = props;
+  const trigger = useScrollTrigger({
+    // target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector(
+      '#back-to-top-anchor',
+    );
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+      });
+    }
+  };
+
+  return (
+    <Zoom in={trigger}>
+      <Box
+        onClick={handleClick}
+        role="presentation"
+        sx={{ position: 'fixed', bottom: 16, right: 16 }}
+      >
+        {children}
+      </Box>
+    </Zoom>
+  );
+}
+
+
+function BackToTop(props) {
+  return (
+    <Fragment>   
+      <ScrollTop {...props}>
+        <Fab color="primary" size="small" aria-label="scroll back to top">
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </ScrollTop>
+    </Fragment>
+  );
+}
+
+export default BackToTop
